4317c30e51c83b54d8b1151014b90b4f7748a4ec
Ryan P. Brewster <RyanPBrewster@gmail.com>
2018-02-16 22:37:16 -0800

    technically playable

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -1,12 +1,15 @@
 extern crate termion;
 
 use termion::color::{Bg, Blue, Color, Green, Red, Reset, White};
+use termion::event::{Event, Key};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 
 extern crate rand;
 use rand::{Rng, SeedableRng, XorShiftRng};
-use rand::distributions::{IndependentSample, Range};
 
 use std::ops::{Index, IndexMut};
+use std::collections::VecDeque;
 
 struct Grid {
     elements: Vec<u8>,
@@ -44,8 +47,8 @@ impl Grid {
     }
 
     fn print(&self) {
-        // print!("{}", termion::clear::All);
-        // print!("{}", termion::cursor::Goto(1, 1));
+        print!("{}", termion::clear::All);
+        print!("{}", termion::cursor::Goto(1, 1));
         for i in 0..self.num_rows {
             for j in 0..self.num_cols {
                 match *self.index((i, j)) {
@@ -59,11 +62,74 @@ impl Grid {
             println!("{}", Bg(Reset));
         }
     }
+
+    fn expand(&mut self, target: u8) {
+        let e0 = self.elements[0];
+        if target == e0 {
+            return;
+        }
+
+        let mut queue = VecDeque::new();
+        queue.push_back((0, 0));
+        while let Some((i, j)) = queue.pop_front() {
+            if self[(i, j)] == e0 {
+                self[(i, j)] = target;
+                if i > 0 {
+                    queue.push_back((i - 1, j));
+                }
+                if i + 1 < self.num_rows {
+                    queue.push_back((i + 1, j));
+                }
+                if j > 0 {
+                    queue.push_back((i, j - 1));
+                }
+                if j + 1 < self.num_cols {
+                    queue.push_back((i, j + 1));
+                }
+            }
+        }
+    }
+
+    fn is_uniform(&self) -> bool {
+        let e0 = self.elements[0];
+        self.elements.iter().all(|&e| e == e0)
+    }
 }
 
 fn main() {
+    let stdin = std::io::stdin();
+    // let mut stdout = std::io::stdout().into_raw_mode().unwrap();
+
     let mut prng = XorShiftRng::from_seed([42, 42, 42, 42]);
     let mut grid: Grid = Grid::random(&mut prng, 10, 10);
     grid[(0, 0)] = Grid::SEED;
+
+    let mut history = Vec::new();
+
     grid.print();
+    for c in stdin.keys() {
+        let evt = c.unwrap();
+        match evt {
+            Key::Char('q') => break,
+            Key::Char('r') => {
+                history.push('r');
+                grid.expand(b'r');
+            }
+            Key::Char('g') => {
+                history.push('g');
+                grid.expand(b'g');
+            }
+            Key::Char('b') => {
+                history.push('b');
+                grid.expand(b'b');
+            }
+            _ => (),
+        }
+        grid.print();
+        if grid.is_uniform() {
+            break;
+        }
+    }
+
+    println!("{:?}", history);
 }
